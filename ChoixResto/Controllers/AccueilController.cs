86d4630982d5cc4107bb60cd6c41967fec0c9a79
QuentinDesableau <quentin.desableau@gmail.com>
2018-03-26 15:15:19 +0200

    Add files via upload

---
+++ ChoixResto/Controllers/AccueilController.cs
@@ -0,0 +1,49 @@
+﻿using ChoixResto.Models;
+using ChoixResto.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChoixResto.Controllers
+{
+    public class AccueilController : Controller
+    {
+        public ActionResult Index ()
+        {
+            return View ();
+        }
+
+        public ActionResult ModifierRestaurant (int? id)
+        {
+            if (id.HasValue)
+            {
+                using (IDal dal = new Dal ())
+                {
+                    Resto restaurant = dal.ObtientTousLesRestaurants ().FirstOrDefault (r => r.Id == id.Value);
+                    if (restaurant == null)
+                    {
+                        return View ("Error");
+                    }
+                    return View (restaurant);
+                }
+            }
+
+            else
+            {
+                return View ("Error");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ModifierRestaurant (Resto resto)
+        {
+            using (IDal dal = new Dal ())
+            {
+                dal.ModifierRestaurant (resto.Id, resto.Nom, resto.Telephone);
+                return RedirectToAction ("Index");
+            }
+        }
+    }
+}
---
+++ ChoixResto/Controllers/RestaurantController.cs
@@ -0,0 +1,21 @@
+﻿using ChoixResto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChoixResto.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (IDal dal = new Dal())
+            {
+                List<Resto> listeDesRestaurants = dal.ObtientTousLesRestaurants();
+                return View(listeDesRestaurants);
+            }
+        }
+    }
+}
